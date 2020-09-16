4f670b720803a215f0b3fe02b18c45596bb6ae9b
Chad Ostrowski <221614+chadoh@users.noreply.github.com>
2020-09-15 21:12:05 -0400

    initial commit: token contract written

---
+++ contracts/AbundanceToken.sol
@@ -0,0 +1,12 @@
+pragma solidity ^0.6.0;
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/token/ERC20/ERC20Burnable.sol";
+
+contract AbundanceToken is ERC20, ERC20Burnable {
+    constructor() public ERC20("Abundance", "ABOUND") {}
+
+    function mint(uint256 amount) public virtual {
+        _mint(_msgSender(), amount);
+    }
+}
